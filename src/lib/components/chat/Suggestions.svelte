3a2e46f6202d0ef82294c96be39417d02105b8a6
lingyicute <160479206+lingyicute@users.noreply.github.com>
2025-02-10 13:47:49 +0000

    up

--- src/lib/components/chat/Suggestions.svelte
+++ src/lib/components/chat/Suggestions.svelte
@@ -65,12 +65,6 @@
 		{$i18n.t('Suggested')}
 	{:else}
 		<!-- Keine Vorschläge -->
-
-		<div
-			class="flex w-full text-center items-center justify-center self-start text-gray-400 dark:text-gray-600"
-		>
-			{$WEBUI_NAME} ‧ v{WEBUI_VERSION}
-		</div>
 	{/if}
 </div>
 
